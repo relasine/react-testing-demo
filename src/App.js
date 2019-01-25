b8f014b3371127cafdb0a10f87330e34a7c9267f
Kevin Simpson <relasine@gmail.com>
2019-01-25 15:34:08 -0700

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ControlledForm from "./ControlledForm/ControlledForm";
 
 class App extends Component {
+  submit = submission => {
+    console.log(submission);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ControlledForm submit={this.submit} />
       </div>
     );
   }
